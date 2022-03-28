509be544bc86012fbe28ee36879494bbad46dc3e
Ashley Battles <ashbatt17@yahoo.com>
2022-03-27 21:24:38 -0400

    inital-update

---
+++ script.js
@@ -0,0 +1,11 @@
+function turnOff(element) {
+    element.innerText = "Log Out";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function myFunctionAlert() {
+    alert("Ninja was liked!");
+}
